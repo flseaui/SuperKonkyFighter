065ea6fef2c8e8ea4b73b99ff695e0522d1e55d6
Emmett Glase <emmettglaser@gmail.com>
2017-09-25 19:52:13 +0000

    No Comments

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
+    public static bool controllable;
+    
 	// Use this for initialization
 	void Start () {
-		
+        if (true)
+        {
+            Destroy(this);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
 	}
 }
