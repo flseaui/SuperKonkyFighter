3bc5da59873740a1cbc3a1a950b44cb0ead7a727
Emmett Glase <emmettglaser@gmail.com>
2017-09-25 19:58:14 +0000

    kek 5.0

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -6,8 +6,10 @@ public class PlayerScript : MonoBehaviour {
 
     public static bool controllable;
     
+    public static kek;
 	// Use this for initialization
 	void Start () {
+
 	}
 	
 	// Update is called once per frame
@@ -24,11 +26,19 @@ public class PlayerScript : MonoBehaviour {
             position.x -= 0.04f;
             this.transform.position = position;
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 position = this.transform.position;
+            position.y -= 0.04f;
+            this.transform.position = position;
+        }
         if (Input.GetKey(KeyCode.D))
         {
             Vector3 position = this.transform.position;
             position.x += 0.04f;
             this.transform.position = position;
         }
+
+
     }
 }
