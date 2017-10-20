7a2ca72896fa828651d58f04a2953d8b5e5260d5
Vincent Migliore <vincenthepic@gmail.com>
2017-10-20 20:18:48 +0000

    No Comments

---
+++ Assets/scripts/UIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    public GameObject player;
+    public Slider health1;
+    public Slider health2;
+    public Slider meter1;
+    public Slider meter2;
+
+    // Use this for initialization
+    void Start () {
+        health1.value = 10000;
+        health2.value = 10000;
+        meter1.value = 0;
+        meter2.value = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+	}
+}
