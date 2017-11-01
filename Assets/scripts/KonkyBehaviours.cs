0602861acf8abf568b49945a464ed973a4df9ca3
Emmett Glaser <emmettglaser@gmail.com>
2017-11-01 20:10:17 +0000

    No Comments

--- Assets/scripts/KonkyBehaviours.cs
+++ Assets/scripts/KonkyBehaviours.cs
@@ -24,7 +24,7 @@ public class KonkyBehaviours : Behaviors{
 
     static  int[] konkyFrames = new int[]
     {
-        15, //KONKY ELBOW 8
+        16, //KONKY ELBOW 8
     };
 
     public KonkyBehaviours() : base(konkyMoveMap, konkyFrames) {}
