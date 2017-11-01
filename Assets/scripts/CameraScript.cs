525b71f6c40ebd45df5e15134b16f577af53df72
Emmett Glaser <emmettglaser@gmail.com>
2017-11-01 19:55:35 +0000

    camera a big skek

--- Assets/scripts/CameraScript.cs
+++ Assets/scripts/CameraScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -46,4 +46,4 @@ public class CameraScript : MonoBehaviour {
         position.x += amm;
         o.transform.position = position;
     }
-}
+}*/
