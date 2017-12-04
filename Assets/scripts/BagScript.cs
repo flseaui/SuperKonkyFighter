0ac25ecaacce6c5f35e828f427e8907c1315cf47
Vincent Migliore <vincenthepic@gmail.com>
2017-12-04 21:48:43 +0000

    No Comments

--- Assets/scripts/BagScript.cs
+++ Assets/scripts/BagScript.cs
@@ -6,10 +6,12 @@ public class BagScript : MonoBehaviour {
 
     public BoxCollider2D hitbox;
     public bool hit;
+    public bool entering;
 
 	// Use this for initialization
 	void Start () {
         hit = false;
+        entering = false;
 	}
 	
 	// Update is called once per frame
@@ -17,9 +19,34 @@ public class BagScript : MonoBehaviour {
 
 	}
 
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("GOTEM");
+        Debug.Log("hit");
         hit = true;
+        
     }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "player")
+        {
+            entering = true;
+            Debug.Log("reeeeeeeeeeeeeee");
+        }
+        else if (col.gameObject.tag != "player" && entering == true)
+        {
+            entering = false;
+            Debug.Log("hit");
+            hit = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "p1HurtBox")
+        {
+            Debug.Log("reeeeeeeeeeeeeee");
+        }
+    }
+
 }
