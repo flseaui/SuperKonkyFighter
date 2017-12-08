a28bd4fe2cb524690b7e8b61015ea036dd8ad808
Vincent Migliore <vincenthepic@gmail.com>
2017-12-08 21:04:07 +0000

    No Comments

--- Assets/scripts/hitboxScript.cs
+++ Assets/scripts/hitboxScript.cs
@@ -14,7 +14,8 @@ public class hitboxScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        hit = false;
+        collide = false;
 	}
 
     private void OnTriggerEnter2D(Collider2D col)
