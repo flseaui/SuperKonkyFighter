0f17f374b252eb51c475c2ae89943576f2729884
Emmett Glaser <emmettglaser@gmail.com>
2017-12-12 21:53:02 +0000

    sd

--- Assets/scripts/hitboxScript.cs
+++ Assets/scripts/hitboxScript.cs
@@ -19,9 +19,14 @@ public class hitboxScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (collide)
+        float h = 12;
+        float w = 6;
+        float y = script.getY();
+        float x = otherScript.getX();
+        float tx = (w / 2) * ((y / h) - 1);
+        if (y<=h && script.getX() < tx && script.facingRight)
         {
-            
+            script.setX(tx);
         }
 	}
 
@@ -69,18 +74,19 @@ public class hitboxScript : MonoBehaviour {
         }
     }*/
 
-    private void OnCollisionStay2D(Collision2D col)
+   /* private void OnCollisionStay2D(Collision2D col)
     {
         if (col.gameObject.tag == "hitbox")
         {
             if (script.facingRight)
             {
-                script.setX(otherScript.getX() - otherScript.hitbox.size.x / 2 - script.hitbox.size.x / 2);
+                script.setX(otherScript.getX() - otherScript.hitbox.size.x / 2 - script.hitbox.size.x / 2 - script.hitbox.offset.x);
             }
             else
             {
-                script.setX(otherScript.getX() + otherScript.hitbox.size.x / 2 + script.hitbox.size.x / 2);
+                script.setX(otherScript.getX() + otherScript.hitbox.size.x / 2 + script.hitbox.size.x / 2 - script.hitbox.offset.x);
             }
         }
-    }
+    }*/
 }
+// script.setX(otherScript.getX() - otherScript.hitbox.size.x / 2 - script.hitbox.size.x / 2 - script.hitbox.offset.x);
