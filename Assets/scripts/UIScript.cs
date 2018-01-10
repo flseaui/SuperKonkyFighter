4ff6f8f41092019a0a3ce1d7383918b4482a2f79
Vincent Migliore <vincenthepic@gmail.com>
2018-01-10 21:08:16 +0000

    No Comments

--- Assets/scripts/UIScript.cs
+++ Assets/scripts/UIScript.cs
@@ -6,17 +6,28 @@ using UnityEngine.UI;
 public class UIScript : MonoBehaviour {
 
     public GameObject player;
+
     public Slider health1;
     public Slider health2;
     public Slider meter1;
     public Slider meter2;
+
+    public GameObject back;
 
     // Use this for initialization
     void Start () {
         health1.value = 10000;
         health2.value = 10000;
         //meter1.value = 0.5f;
-       // meter2.value = 0.5f;
+        // meter2.value = 0.5f;
+
+        Vector3 position = back.transform.position;
+        position.x = 0;
+        back.transform.position = position;
+
+        position = back.transform.position;
+        position.y = 23.5f;
+        back.transform.position = position;
     }
 	
 	// Update is called once per frame
