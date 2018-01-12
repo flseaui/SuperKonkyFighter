bbe3549ec2fb6c827f59a486a3433abd3226d1c0
scarabbeetlearmy@gmail.com <scarabbeetlearmy@gmail.com>
2018-01-12 18:51:51 +0000

    started adding hitstun values

--- Assets/scripts/KonkyBehaviours.cs
+++ Assets/scripts/KonkyBehaviours.cs
@@ -38,22 +38,29 @@ public class KonkyBehaviours : Behaviors{
 
 	static int[,] konkyFrames = new int[,]
     {
-        {15,  8},//5L  0
-        {20, 12},//5M  1
-        {34, 18},//6M  2
-        {30, 17},//5H  3
-        {19, 10},//2L  4
-        {24, 13},//2M  5
-        {20, 23},//2H  6
-        {16,  7},//jL  7
-        {26, 13},//jM  8
-        {31, 17},//jH  9
-        {52, 40},//1S  10
-        {26, 13},//6S  11
-        {34, 20},//3S  12
-        {53, 33},//2S  13
-		{999, 989},//4S
+        {15,  8},//5L  0  | Knockback: , Angle: , Damage: , Meter: , Level: 0, P1 Scaling: 1
+        {20, 12},//5M  1  | Knockback: , Angle: , Damage: , Meter: , Level: 1, P1 Scaling: 1
+        {34, 18},//6M  2  | Knockback: , Angle: , Damage: , Meter: , Level: 0, P1 Scaling: 1.1
+        {30, 17},//5H  3  | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: 1
+        {19, 10},//2L  4  | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: 1
+        {24, 13},//2M  5  | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: 1
+        {20, 23},//2H  6  | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: 1
+        {16,  7},//jL  7  | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: .8
+        {26, 13},//jM  8  | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: .8
+        {31, 17},//jH  9  | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: .8
+        {52, 40},//1S  10 | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: 1
+        {26, 13},//6S  11 | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: 1
+        {34, 20},//3S  12 | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: 1
+        {53, 33},//2S  13 | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: 1
+		{999, 989},//4S   | Knockback: , Angle: , Damage: , Meter: , Level: , P1 Scaling: 1
     };
 
     public KonkyBehaviours() : base(konkyMoveMap, konkyFrames) {}
 }
+//Level | Hitstop | Hitstun | Blockstun | Scaling
+//0     | 8       | 12      | 9         | .75
+//1     | 10      | 14      | 11        | .8
+//2     | 12      | 16      | 13        | .85
+//3     | 14      | 19      | 16        | .89
+//4     | 16      | 21      | 18        | .92
+//5     | 18      | 24      | 20        | .94
