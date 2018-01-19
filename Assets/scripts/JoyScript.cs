f0d371fdc864cbb5a4cf1ab4bdaf26f6e32abbfc
Vincent Migliore <vincenthepic@gmail.com>
2018-01-19 21:06:11 +0000

    No Comments

---
+++ Assets/scripts/JoyScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class JoyScript : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+
+    private Image bgImg;
+    private Image joystick;
+    private Vector3 input;
+
+
+    private void start()
+    {
+        Debug.Log("ran");
+
+        bgImg = GetComponent<Image>();
+        joystick = transform.GetChild(0).GetComponent<Image>();
+
+    }
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Debug.Log("ran");
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+
+            input = new Vector3(pos.x * 2 + 1, 0, pos.y * 2 - 1);
+            input = (input.magnitude > 1.0f) ? input.normalized : input;
+
+            joystick.rectTransform.anchoredPosition = new Vector3(input.x * (bgImg.rectTransform.sizeDelta.x / 2), (input.z * (bgImg.rectTransform.sizeDelta.y / 2)));
+
+
+        }
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+        Debug.Log("ran");
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        Debug.Log("ran");
+    }
+}
