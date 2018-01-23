e6c51969800f118808e0d80c04e74dce4859c58d
Vincent Migliore <vincenthepic@gmail.com>
2018-01-23 21:26:56 +0000

    JoyStick is now functional

--- Assets/scripts/JoyScript.cs
+++ Assets/scripts/JoyScript.cs
@@ -4,48 +4,50 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class JoyScript : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+public class JoyScript : MonoBehaviour
 {
 
     private Image bgImg;
     private Image joystick;
     private Vector3 input;
+    public Camera Cam;
+    private bool clicked;
 
 
     private void Start()
     {
-        Debug.Log("ran");
         bgImg = GetComponent<Image>();
         joystick = transform.GetChild(0).GetComponent<Image>();
 
     }
 
-    public void OnDrag(PointerEventData ped)
+    private void Update()
     {
-        Debug.Log("ran");
-        Vector2 pos;
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        if (Input.GetMouseButtonDown(0) && Input.mousePosition.x < 350 && Input.mousePosition.y < 300)
         {
-            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
-            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
-
-            input = new Vector3(pos.x * 2 + 1, 0, pos.y * 2 - 1);
-            input = (input.magnitude > 1.0f) ? input.normalized : input;
-
-            joystick.rectTransform.anchoredPosition = new Vector3(input.x * (bgImg.rectTransform.sizeDelta.x / 2), (input.z * (bgImg.rectTransform.sizeDelta.y / 2)));
-
-
+            clicked = true;
         }
-    }
+        if (Input.GetMouseButton(0) && clicked)
+        {
+            Debug.Log("worked");
+            Vector2 pos;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, Input.mousePosition, Cam, out pos))
+            {
+                pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+                pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
 
-    public void OnPointerDown(PointerEventData ped)
-    {
-        OnDrag(ped);
-        Debug.Log("ran");
-    }
+                input = new Vector3(pos.x * 2, 0, pos.y * 2);
+                input = (input.magnitude > 1.0f) ? input.normalized : input;
 
-    public void OnPointerUp(PointerEventData ped)
-    {
-        Debug.Log("ran");
+                joystick.rectTransform.anchoredPosition = new Vector3(input.x * (bgImg.rectTransform.sizeDelta.x / 2), (input.z * (bgImg.rectTransform.sizeDelta.y / 2)));
+
+
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            joystick.rectTransform.anchoredPosition = new Vector3(0, 0);
+            clicked = false;
+        }
     }
 }
