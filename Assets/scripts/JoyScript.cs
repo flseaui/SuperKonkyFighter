e442070957e775c0b65c04a2807a656f3eef2846
Vincent Migliore <vincenthepic@gmail.com>
2018-01-28 00:23:35 +0000

    No Comments

--- Assets/scripts/JoyScript.cs
+++ Assets/scripts/JoyScript.cs
@@ -11,21 +11,31 @@ public class JoyScript : MonoBehaviour
     private Image joystick;
     private Vector3 input;
     public Camera Cam;
+
     private bool clicked;
+
     private float joyx;
     private float joyy;
+
+    private float fullr;
+
     public bool Up;
     public bool Right;
     public bool Down;
     public bool Left;
 
+
     private void Start()
     {
         bgImg = GetComponent<Image>();
         joystick = transform.GetChild(0).GetComponent<Image>();
+
+        RectTransform rt = (RectTransform)bgImg.transform;
+
+        fullr = rt.rect.height/30;
+
         joyx = joystick.transform.position.x;
         joyy = joystick.transform.position.y;
-
     }
 
     private void Update()
@@ -47,9 +57,13 @@ public class JoyScript : MonoBehaviour
 
                 joystick.rectTransform.anchoredPosition = new Vector3(input.x * (bgImg.rectTransform.sizeDelta.x / 2), (input.z * (bgImg.rectTransform.sizeDelta.y / 2)));
 
-                float angle = (Mathf.Atan2((joystick.transform.position.y - joyy), (joystick.transform.position.x - joyx))/Mathf.PI)*180;
-                Debug.Log(angle);
-                Dir(angle);
+                float y = (joystick.transform.position.y - joyy);
+                float x = (joystick.transform.position.x - joyx);
+
+                float angle = (Mathf.Atan2(y, x)/Mathf.PI)*180;
+                float radius = Mathf.Sqrt((y * y) + (x * x));
+
+                Dir(angle,radius);
 
             }
         }
@@ -57,44 +71,58 @@ public class JoyScript : MonoBehaviour
         {
             joystick.rectTransform.anchoredPosition = new Vector3(0, 0);
             clicked = false;
+            Left = false;
+            Right = false;
+            Up = false;
+            Down = false;
         }
     }
 
-    public void Dir(float a)
+    public void Dir(float a, float r)
     {
-        if(a > 112.5 || a < -112.5)
+        if (r >= fullr / 3)
         {
-            Left = true; 
+            if (a > 112.5 || a < -112.5)
+            {
+                Left = true;
+            }
+            else
+            {
+                Left = false;
+            }
+
+            if (a < 67.5 && a > -67.5)
+            {
+                Right = true;
+            }
+            else
+            {
+                Right = false;
+            }
+
+            if (a < 157.5 && a > 22.5)
+            {
+                Up = true;
+            }
+            else
+            {
+                Up = false;
+            }
+
+            if (a > -157.5 && a < -22.5)
+            {
+                Down = true;
+            }
+            else
+            {
+                Down = false;
+            }
         }
         else
         {
             Left = false;
-        }
-
-        if (a < 67.5 && a > -67.5)
-        {
-            Right = true;
-        }
-        else
-        {
             Right = false;
-        }
-
-        if (a < 157.5 && a > 22.5)
-        {
-            Up = true;
-        }
-        else
-        {
             Up = false;
-        }
-
-        if (a > -157.5 && a < -22.5)
-        {
-            Down = true;
-        }
-        else
-        {
             Down = false;
         }
     }
