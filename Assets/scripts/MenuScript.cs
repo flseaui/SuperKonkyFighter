e8b14bc738cf33453c7df1bcd8af411d40053b32
Emmett Glaser <emmettglaser@gmail.com>
2018-02-15 22:08:45 +0000

    No Comments

--- Assets/scripts/MenuScript.cs
+++ Assets/scripts/MenuScript.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MenuScript : MonoBehaviour {
+
+	public GameObject buttonPrefab;
+	public Canvas canvas;
+
+	public int screen;
 
 	// Use this for initialization
 	void Start () {
@@ -11,18 +18,50 @@ public class MenuScript : MonoBehaviour {
         PlayerPrefs.SetInt("ground", 0);
 		PlayerPrefs.SetInt("character1", 0);
 		PlayerPrefs.SetInt("character2", 0);
+
+		GameObject playButton = Instantiate(buttonPrefab);
+		//playButton.transform.SetParent(canvas.transform);
+		Vector3 v = playButton.transform.position;
+		v.z = 0;
+		playButton.transform.position = v;
+		v = playButton.transform.localScale;
+		v.x = 1 / 45f;
+		v.y = 1 / 45f;
+		v.z = 1 / 45f;
+		v= playButton.transform.localScale;
+		playButton.GetComponent<Button>().onClick.AddListener(beginGame);
 	}
 	
 	// Update is called once per frame
-	void OnGUI () {
-
-		if (GUI.Button(new Rect(200, 240, 200, 60), "play"))
+	void Update () {
+		//if ()
 		{
-			SceneManager.LoadScene("SKF");
-			int stage = Random.Range(1,2);
-			PlayerPrefs.SetInt("background", stage);
-			PlayerPrefs.SetInt("ground", stage);
+			
 		}
+		
+			
+		
 
     }
+
+	public void startScreen(int no)
+	{
+		switch (no) {
+			case 0:
+				//GameObject playbutton = Instantiate(buttonPrefab);
+				break;
+			default:
+				//GameObject playbutton = Instantiate(buttonPrefab);
+				break;
+		}
+	}
+
+	public void beginGame()
+	{
+		SceneManager.LoadScene("SKF");
+		int stage = Random.Range(1, 2);
+		PlayerPrefs.SetInt("background", stage);
+		PlayerPrefs.SetInt("ground", stage);
+	}
+
 }
