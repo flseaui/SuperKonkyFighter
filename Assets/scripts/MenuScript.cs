c1a53171556b72203b6367bdeb53dbc2f4182e85
Emmett Glaser <emmettglaser@gmail.com>
2018-02-15 18:51:28 +0000

    dsafsdaf

--- Assets/scripts/MenuScript.cs
+++ Assets/scripts/MenuScript.cs
@@ -9,36 +9,20 @@ public class MenuScript : MonoBehaviour {
 	void Start () {
         PlayerPrefs.SetInt("background", 0);
         PlayerPrefs.SetInt("ground", 0);
-    }
+		PlayerPrefs.SetInt("character1", 0);
+		PlayerPrefs.SetInt("character2", 0);
+	}
 	
 	// Update is called once per frame
 	void OnGUI () {
-        if (GUI.Button(new Rect(10, 30, 200, 60), "background1"))
-        {
-            SceneManager.LoadScene("SKF");
 
-        }
-        if (GUI.Button(new Rect(210, 30, 200, 60), "background2"))
-        {
-            SceneManager.LoadScene("SKF");
-            PlayerPrefs.SetInt("background", 1);
-            PlayerPrefs.SetInt("ground", 1);
-        }
-        if (GUI.Button(new Rect(410, 30, 200, 60), "background3"))
-        {
-            SceneManager.LoadScene("SKF");
-            PlayerPrefs.SetInt("background", 2);
-        }
-        if (GUI.Button(new Rect(10, 230, 200, 60), "background4"))
-        {
-            SceneManager.LoadScene("SKF");
-            PlayerPrefs.SetInt("background", 3);
-        }
-        if (GUI.Button(new Rect(10, 430, 200, 60), "background5"))
-        {
-            SceneManager.LoadScene("SKF");
-            PlayerPrefs.SetInt("background", 4);
-        }
+		if (GUI.Button(new Rect(200, 240, 200, 60), "play"))
+		{
+			SceneManager.LoadScene("SKF");
+			int stage = Random.Range(1,2);
+			PlayerPrefs.SetInt("background", stage);
+			PlayerPrefs.SetInt("ground", stage);
+		}
 
     }
 }
