bcb3e0311228785bc337505b6b16b843297d5c2f
Claire Meyer <sploing1@gmail.com>
2018-03-08 07:12:58 +0000

    pushing no longer works

--- Assets/scripts/PeeinWithMyBoyScottDesuNe.cs
+++ Assets/scripts/PeeinWithMyBoyScottDesuNe.cs
@@ -7,8 +7,13 @@ public class PeeinWithMyBoyScottDesuNe : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D col)
     {
+        // this broke pushing for SOME reason I have no idea
+        // was in attempt to fix collision, realized the issue was diamond blocking all hurtbox collision, messed with layers and changed them back
+        // now things are dead
         Debug.Log("enterer:");
-        if ((this.CompareTag("hitbox1") && col.collider.CompareTag("hurtbox2")) || (this.CompareTag("hitbox2") && col.collider.CompareTag("hurtbox1")))
+        Debug.Log("this tag: " + tag);
+        Debug.Log("that tag: " + col.collider.tag);
+        if ((this.CompareTag("hitbox1") && col.collider.CompareTag("hurtbox2")) || ((this.CompareTag("hitbox2") && col.collider.CompareTag("hurtbox1"))))
         {
             hitting = true;
             Debug.Log("hittin with ya boy scott");
