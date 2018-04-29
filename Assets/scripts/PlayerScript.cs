4c592bc21fdb43240afede2bff80e3a208906df5
scarabbeetlearmy@gmail.com <scarabbeetlearmy@gmail.com>
2018-04-29 03:17:10 +0000

    No Comments

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -11,6 +11,8 @@ public class PlayerScript : MonoBehaviour
     private float FLOOR_HEIGHT = 0;
     private float BASE_GRAVITY = -0.05f;
 
+    public double[] dab = new double[2];
+
     private float[,] levelScaling = new float[,]
     {
         //Hitstun | CH Hitstun | Untech Time | CH Untech Time | Hitstop	| CH Hitstop | Blockstun | P2 Scaling
@@ -1019,7 +1021,9 @@ Level Hitstun CH Hitstun Untech Time CH Untech Time	Hitstop	CH Hitstop Blockstun
         else
         {
             damageDealt = true;
-            health -= scaling(damage, p1scaling, levelScaling[level][8]);
+            scaling(damage, p1scaling, levelScaling[level][8]);
+            int scaledDamage = System.Convert.ToInt32(System.Math.Floor(dab[0]));
+            health -= scaledDamage;
             stun();
         }
 
@@ -1092,12 +1096,11 @@ Level Hitstun CH Hitstun Untech Time CH Untech Time	Hitstop	CH Hitstop Blockstun
         return new Vector2(this.transform.position.x, this.transform.position.y);
     }
 
-    public double scaling(int damage, float p1scaling, float levelScaling, double scaleVal)
+    public List<double> scaling(int damage, float p1scaling, float levelScaling, double scaleVal)
     {
         double damageDecimal = damage;
         double scaledDamage = damageDecimal * .6  * p1scaling * scaleVal;
         scaleVal = scaleVal * levelScaling;
-        double[] dab = new double[2];
         dab[0] = scaledDamage;
         dab[1] = scaleVal;
         return dab[];
