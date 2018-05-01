0bcfcd0332af9d5f0083e05f25c174702084ebe6
Vincent Migliore <vincenthepic@gmail.com>
2018-05-01 06:29:42 +0000

    No Comments

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -667,6 +667,9 @@ Level Hitstun CH Hitstun Untech Time CH Untech Time	Hitstop	CH Hitstop Blockstun
                 }
             }
         }
+
+        basicBoxes();
+
     }
 
     private void basicBoxes()
