7b757d8d525dec9db19717ec1920ba84b5369578
Claire Meyer <sploing1@gmail.com>
2018-05-01 05:27:41 +0000

    added color change training stage

---
+++ Assets/scripts/ColorChangeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeScript : MonoBehaviour
+{
+
+    public Gradient gradient;
+    public float strobeDuration = 2f;
+
+    public SpriteRenderer sr;
+
+    private void Start()
+    {
+        StartCoroutine("UpdateColor");
+    }
+
+    IEnumerator UpdateColor()
+    {
+        float t = Mathf.PingPong(Time.time / strobeDuration, 1f);
+        sr.color = gradient.Evaluate(t);
+        
+        yield return null;
+    }
+
+    private void Update()
+    {
+        StartCoroutine("UpdateColor");
+    }
+}
