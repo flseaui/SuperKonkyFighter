fca747469da393bfa41194d166fe8ec82163f02b
Claire Meyer <sploing1@gmail.com>
2018-05-10 17:51:44 +0000

    i beat my children yeet

---
+++ Assets/scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        transform.position += new Vector3(speed, 0, 0);
+	}
+
+    void 
+}
