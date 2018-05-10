9955778e35b4ad0b9f9cfdae10d7631c96b5b830
Vincent Migliore <vincenthepic@gmail.com>
2018-05-10 03:47:54 +0000

    No Comments

--- Assets/scripts/KonkyBehaviours.cs
+++ Assets/scripts/KonkyBehaviours.cs
@@ -263,7 +263,7 @@ public class KonkyBehaviours : Behaviors
         p1scaling = 1f,
         block = MID,
         knockdown = NONE,
-        actionCancels = new int[] { 12, 25, 31, 32, 33, 34, 35, 36 },
+        actionCancels = new int[] { 12, 22, 25, 31, 32, 33, 34, 35, 36 },
         gAngle = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
         gStrength = new float[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
         aAngle = new int[] { 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30 },
