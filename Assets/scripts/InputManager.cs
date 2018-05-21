081310b486edb0f06ed404e71961cde1ae2ec262
Claire Meyer <sploing1@gmail.com>
2018-05-21 01:54:53 +0000

    input skek

--- Assets/scripts/InputManager.cs
+++ Assets/scripts/InputManager.cs
@@ -17,6 +17,7 @@ public class InputManager
                 medium = false,
                 heavy = false,
                 special = false;
+      
 
     JoyScript joyScript;
 
@@ -33,10 +34,16 @@ public class InputManager
                   mediumButton,
                   heavyButton;
 
-    public float hAxis, vAxis, dhAxis, dvAxis, aAxis, htAxis, vtAxis;
-    public float hAxisPrev, vAxisPrev, dhAxisPrev, dvAxisPrev;
+    public float hAxis, vAxis, dhAxis, dvAxis, 
+                 hAxisPrev, vAxisPrev, dhAxisPrev, dvAxisPrev;
+
+    public bool x, y, a, b, rb;
+
+    int playerID;
+
     public InputManager(int playerID)
     {
+        this.playerID = playerID;
         if (playerID == 1)
         {
             upKey = KeyCode.W;
@@ -61,40 +68,6 @@ public class InputManager
         }
     }
 
-    public bool justPressed(string direction)
-    {
-        switch (direction)
-        {
-            case "left":
-                return (hAxisPrev == 0 && hAxis < 0) || (dhAxisPrev == 0 && dhAxis < 0);
-            case "right":
-                return (hAxisPrev == 0 && hAxis > 0) || (dhAxisPrev == 0 && dhAxis > 0);
-            case "up":
-                return (vAxisPrev == 0 && vAxis < 0) || (dvAxisPrev == 0 && dvAxis > 0);
-            case "down":
-                return (vAxisPrev == 0 && vAxis > 0) || (dvAxisPrev == 0 && dvAxis < 0);
-            default:
-                return false;
-        }
-    }
-
-    public bool justReleased(string direction)
-    {
-        switch (direction)
-        {
-            case "left":
-                return (hAxisPrev < 0 && hAxis == 0) || (dhAxisPrev < 0 && dhAxis == 0);
-            case "right":
-                return (hAxisPrev > 0 && hAxis == 0) || (dhAxisPrev > 0 && dhAxis == 0);
-            case "up":
-                return (vAxisPrev < 0 && vAxis == 0) || (dvAxisPrev > 0 && dvAxis == 0);
-            case "down":
-                return (vAxisPrev > 0 && vAxis == 0) || (dvAxisPrev < 0 && dvAxis == 0);
-            default:
-                return false;
-        }
-    }
-
     /* 
      * Polls user input
      * inputType = 0 : keyboard
@@ -104,18 +77,36 @@ public class InputManager
     {
         if (isInputEnabled)
         {
+            foreach (string name in Input.GetJoystickNames())
+                Debug.Log(name);
+
             hAxisPrev = hAxis;
             vAxisPrev = vAxis;
             dhAxisPrev = dhAxis;
             dvAxisPrev = dvAxis;
-            hAxis = Input.GetAxis("Horizontal");
-            vAxis = Input.GetAxis("Vertical");
-            htAxis = Input.GetAxis("HorizontalTurn");
-            vtAxis = Input.GetAxis("VerticalTurn");
-            dhAxis = Input.GetAxis("XboxDpadHorizontal");
-            dvAxis = Input.GetAxis("XboxDpadVertical");
 
-            ControllerCheck();
+            if (playerID == 1)
+            {
+                hAxis = Input.GetAxis("Horizontal");
+                vAxis = Input.GetAxis("Vertical");
+                dhAxis = Input.GetAxis("XboxDpadHorizontal");
+                dvAxis = Input.GetAxis("XboxDpadVertical");
+                x = (Input.GetButton("XboxX"));
+                y = (Input.GetButton("XboxY"));
+                rb = (Input.GetButton("XboxRB"));
+                a = (Input.GetButton("XboxA"));
+            }
+            else if (playerID == 2)
+            {
+                hAxis = Input.GetAxis("Horizontal_2");
+                vAxis = Input.GetAxis("Vertical_2");
+                dhAxis = Input.GetAxis("XboxDpadHorizontal_2");
+                dvAxis = Input.GetAxis("XboxDpadVertical_2");
+                x = (Input.GetButton("XboxX_2"));
+                y = (Input.GetButton("XboxY_2"));
+                rb = (Input.GetButton("XboxRB_2"));
+                a = (Input.GetButton("XboxA_2"));
+            }
 
             switch (inputType)
             {
@@ -140,10 +131,10 @@ public class InputManager
                     currentInput[1] = vAxis > 0 || dvAxis < 0;
                     currentInput[2] = hAxis < 0 || dhAxis < 0;
                     currentInput[3] = hAxis > 0 || dhAxis > 0;
-                    currentInput[4] = (Input.GetButton("XboxX"));
-                    currentInput[5] = (Input.GetButton("XboxY"));
-                    currentInput[6] = (Input.GetButton("XboxRB"));
-                    currentInput[7] = (Input.GetButton("XboxA"));
+                    currentInput[4] = x;
+                    currentInput[5] = y;
+                    currentInput[6] = a;
+                    currentInput[7] = b;
                     currentInput[8] = justPressed("left");
                     currentInput[9] = justPressed("right");
                     currentInput[10] = justReleased("left");
@@ -181,23 +172,38 @@ public class InputManager
         }
     }
 
-    void ControllerCheck()
+    public bool justPressed(string direction)
     {
-        float ltaxis = Input.GetAxis("XboxLeftTrigger");
-        float rtaxis = Input.GetAxis("XboxRightTrigger");
+        switch (direction)
+        {
+            case "left":
+                return (hAxisPrev == 0 && hAxis < 0) || (dhAxisPrev == 0 && dhAxis < 0);
+            case "right":
+                return (hAxisPrev == 0 && hAxis > 0) || (dhAxisPrev == 0 && dhAxis > 0);
+            case "up":
+                return (vAxisPrev == 0 && vAxis < 0) || (dvAxisPrev == 0 && dvAxis > 0);
+            case "down":
+                return (vAxisPrev == 0 && vAxis > 0) || (dvAxisPrev == 0 && dvAxis < 0);
+            default:
+                return false;
+        }
+    }
 
-        bool xbox_a = Input.GetButton("XboxA");
-        bool xbox_b = Input.GetButton("XboxB");
-        bool xbox_x = Input.GetButton("XboxX");
-        bool xbox_y = Input.GetButton("XboxY");
-        bool xbox_lb = Input.GetButton("XboxLB");
-        bool xbox_rb = Input.GetButton("XboxRB");
-        bool xbox_ls = Input.GetButton("XboxLS");
-        bool xbox_rs = Input.GetButton("XboxRS");
-        bool xbox_view = Input.GetButton("XboxView");
-        bool xbox_menu = Input.GetButton("XboxMenu");
-
-        //Debug.LogFormat("h: {0}, v: {1}", dhAxis, dvAxis);
+    public bool justReleased(string direction)
+    {
+        switch (direction)
+        {
+            case "left":
+                return (hAxisPrev < 0 && hAxis == 0) || (dhAxisPrev < 0 && dhAxis == 0);
+            case "right":
+                return (hAxisPrev > 0 && hAxis == 0) || (dhAxisPrev > 0 && dhAxis == 0);
+            case "up":
+                return (vAxisPrev < 0 && vAxis == 0) || (dvAxisPrev > 0 && dvAxis == 0);
+            case "down":
+                return (vAxisPrev > 0 && vAxis == 0) || (dvAxisPrev < 0 && dvAxis == 0);
+            default:
+                return false;
+        }
     }
 
     // Handles player input
