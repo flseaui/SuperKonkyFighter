a43359cdb8b8a449dfa7c29a0f1d3b5a95a53b81
Claire Meyer <sploing1@gmail.com>
2018-05-22 00:16:28 +0000

    did some projectile fixes

--- Assets/scripts/Projectile.cs
+++ Assets/scripts/Projectile.cs
@@ -9,12 +9,16 @@ public class Projectile : MonoBehaviour {
     public bool faceWhenShot = default(bool);
     public int shotAction;
 
+    private GameObject stageLeft, stageRight;
+
     public PlayerScript player { get; set; } = default(PlayerScript);
 
     private void Start()
     {
         faceWhenShot = player.facingRight;
         shotAction = player.executingAction;
+        stageLeft = GameObject.FindWithTag("stageLeft");
+        stageRight = GameObject.FindWithTag("stageRight");
     }
 
     void Update()
@@ -22,11 +26,13 @@ public class Projectile : MonoBehaviour {
         transform.position += faceWhenShot ? new Vector3(speed, 0, 0) : new Vector3(-speed, 0, 0);
 
         GetComponent<SpriteRenderer>().flipX = faceWhenShot ? false : true;
+
+        if (transform.position.x < stageLeft.transform.position.x || transform.position.x > stageRight.transform.position.x)
+            Destroy(this.gameObject);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("faggot____");
         if (collision.gameObject.tag.Equals("projectile"))
         {
             Destroy(collision.gameObject);
@@ -36,23 +42,24 @@ public class Projectile : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (this?.player)
+        if (this.player != null)
         {
             if (!col.transform.parent.parent.tag.Equals(player.tag))
             {
                 player.otherPlayer.GetComponent<PlayerScript>().health -= strength;
-                Destroy(this.gameObject);
                 PlayerScript other = player.otherPlayer.GetComponent<PlayerScript>();
                 Action action = player.behaviors.getAction(shotAction);
-                if (!other.airborn)
+                Destroy(this.gameObject);
+                if (action != null)
                 {
-                    if (action != null)
-                        other.overrideDamage(action.projectileStrength, action.gStrength[player.actionFrameCounter], action.gAngle[player.actionFrameCounter], action.block, action.p1scaling, action.tier, shotAction);
-                }
-                else
-                {
-                    if (action != null)
+                    if (other.airborn)
+                    {
                         other.overrideDamage(action.projectileStrength, action.aStrength[player.actionFrameCounter], action.aAngle[player.actionFrameCounter], action.block, action.p1scaling, action.tier, shotAction);
+                    }
+                    else
+                    {
+                        other.overrideDamage(action.projectileStrength, action.gStrength[player.actionFrameCounter], action.gAngle[player.actionFrameCounter], action.block, action.p1scaling, action.tier, shotAction);
+                    }
                 }
             }
         }
