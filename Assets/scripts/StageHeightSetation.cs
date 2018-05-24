1b9eb68eec0e7df1440d43c07179fda62ac061fe
Claire Meyer <sploing1@gmail.com>
2018-05-24 17:32:42 +0000

    stage fixes

--- Assets/scripts/StageHeightSetation.cs
+++ Assets/scripts/StageHeightSetation.cs
@@ -6,12 +6,12 @@ public class StageHeightSetation : MonoBehaviour {
 
 	void Start () {
         
-        if (PlayerPrefs.GetInt("stage") <= 1)
+        if (PlayerPrefs.GetInt("stage") <= 2)
             transform.position = new Vector3(0, 35.2f, 65);
         else if (PlayerPrefs.GetInt("stage") == 4)
             transform.position = new Vector3(0, 50, 65);
         else
-            transform.position = new Vector3(0, 12, 65);
+            transform.position = new Vector3(0, 35.2f, 65);
     }
 
 }
